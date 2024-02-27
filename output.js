e9a6eb5f1dc96ae761f0fa2fd7570af07da39259
fjkmg <fuji.kana13@gmail.com>
2024-02-27 18:57:52 +0900

    全画面表示モードを追加

--- output.js
+++ output.js
@@ -1,5 +1,8 @@
 window.addEventListener("DOMContentLoaded", ()=>{
+    // DOMContentLoaded を親ウィンドウに知らせる
     window.opener.postMessage("DOMContentLoaded", "http://127.0.0.1:5500")
+    
+    // ウィンドウリサイズ時の処理
     let output = document.getElementById("output");
     let scale = 1;
     function resize(){
@@ -8,4 +11,15 @@ window.addEventListener("DOMContentLoaded", ()=>{
     }
     window.addEventListener("resize", resize, false);
     resize();
+
+    // フルスクリーン表示
+    const overlay = document.getElementById("overlay");
+    overlay.addEventListener("click", ()=>{
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+        } else if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }   
+    });
+    document.addEventListener("fullscreenchange",()=>{overlay.classList.add("nodisplay")})
 })
